252eab4f51abc501f6068b8f70470f51428a787b
pekar.richard <pekar.richard@gmail.com>
2020-07-05 12:23:48 +0200

    uprava store

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,3 +4,4 @@ export const API_ENDPOINT = "http://localhost:8080";
 export const API_ENDPOINTHEADER = "http://localhost:3000";
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = API_ENDPOINT;
+export const DEBUG_MODE = false;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,36 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { DEBUG_MODE } from "./config";
+const { detect } = require("detect-browser");
+const browser = detect();
 
 const initalState = {};
 const middleware = [thunk];
 
 let store;
 
-if (window.navigator.userAgent.includes("Chrome")) {
+if (browser) {
+  console.log(browser.name);
+  console.log(browser.version);
+  console.log(browser.os);
+}
+
+// handle the case where we don't detect the browser
+switch (browser && browser.name) {
+  case "chrome":
+    console.log("supported");
+    break;
+
+  case "firefox":
+    console.log("kinda ok");
+    break;
+
+  default:
+    console.log("not supported");
+}
+
+if (DEBUG_MODE) {
   store = createStore(
     rootReducer,
     initalState,
@@ -24,12 +47,5 @@ if (window.navigator.userAgent.includes("Chrome")) {
     compose(applyMiddleware(...middleware))
   );
 }
-
-/*
-store = createStore(
-  rootReducer,
-  initalState,
-  compose(applyMiddleware(...middleware))
-);*/
 
 export default store;
